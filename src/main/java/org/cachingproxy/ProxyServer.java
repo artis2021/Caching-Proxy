a1522c8ca3d2b1e8bec4355979c2c507c5f4a671
Arti Sahu <arti.sahu@firsthive.com>
2025-01-14 17:52:42 +0530

    Add ProxyServer implementation to start an HTTP proxy server

--- src/main/java/org/cachingproxy/ProxyServer.java
+++ src/main/java/org/cachingproxy/ProxyServer.java
@@ -1,7 +1,27 @@
 package org.cachingproxy;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
 public class ProxyServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter port: ");
+        int port = scanner.nextInt();
+
+        System.out.print("Enter Proxy Server URL: ");
+        String proxy = scanner.next();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.start();
+
+        System.out.println("Proxy Server started on port " + port + " forwarding requests to " + proxy);
+
+
 
     }
 }

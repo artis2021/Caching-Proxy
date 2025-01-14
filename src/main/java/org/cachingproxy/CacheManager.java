f02a7ddb250311466b73f1469331f6e7f273716d
Arti Sahu <arti.sahu@firsthive.com>
2025-01-14 18:09:08 +0530

    Added CacheManager's functalities

--- src/main/java/org/cachingproxy/CacheManager.java
+++ src/main/java/org/cachingproxy/CacheManager.java
@@ -5,5 +5,21 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CacheManager {
     private ConcurrentHashMap<String, CacheObject> caches;
 
+    public CacheManager() {
+        this.caches = new ConcurrentHashMap<>();
+    }
+
+    public void clearCache() {
+        this.caches.clear();
+        System.out.println("Cache cleared");
+    }
+
+    public CacheObject getCache(String key) {
+        return this.caches.get(key);
+    }
+
+    public void addCache(String key, CacheObject value) {
+        this.caches.put(key, value);
+    }
 
 }
